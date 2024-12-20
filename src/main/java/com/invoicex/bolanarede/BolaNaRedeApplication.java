b371d371cf9ebf3d0a81535e83d75761465fa517
Maria Eduarda <mariaeduarda050501@gmail.com>
2024-12-20 17:56:04 -0300

    Projeto criado

---
+++ src/main/java/com/invoicex/bolanarede/BolaNaRedeApplication.java
@@ -0,0 +1,13 @@
+package com.invoicex.bolanarede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaNaRedeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolaNaRedeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/invoicex/bolanarede/BolaNaRedeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoicex.bolanarede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolaNaRedeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
